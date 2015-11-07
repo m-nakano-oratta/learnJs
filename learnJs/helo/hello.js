7e2106537c77fa09126e194bdc99e1d35f140e93
nakanomi <nakanomi@gmail.com>
2015-11-07 18:44:29 +0900

    ls -l と ls -F の標準出力をテスト

--- learnJs/helo/hello.js
+++ learnJs/helo/hello.js
@@ -2,4 +2,20 @@ console.log("hello world");
 var exec = require('child_process').exec;
 exec('ls -l ./', function(err, stdout, stderr) {
 	console.log("stdout:" + stdout);
+
+	files = stdout.split("\n");
+	console.log("count" + files.length);
+	for (i = 0; i < files.length; i++) {
+		console.log("[" + i + "]:" + files[i]);
+	}
 });
+
+exec('ls -F', function(err, stdout, stderr) {
+	files = stdout.split("\n");
+	console.log("count" + files.length);
+	for (i = 0; i < files.length; i++) {
+		console.log("[" + i + "]:" + files[i]);
+	}
+});
+
+console.log("test");
